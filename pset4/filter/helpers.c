eae9050b40d622c36b275600d947e9e1c48d33a3
Dennis Dinh Hung <dennisdinhhung@gmail.com>
2021-08-07 20:44:08 +0700

    finished helpers.c

--- pset4/filter/helpers.c
+++ pset4/filter/helpers.c
@@ -67,155 +67,32 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     // temp for the rounded, blurred pixel
     RGBTRIPLE temp[height][width];
     RGBTRIPLE rounded;
+    
+    
 
     for (int i=0; i < height; i++){
-        for (int j=0; j < width; j++){
-            
-            // init the sums
+        for(int j=0; j < width; j++){
             float sum_red=0, sum_green=0, sum_blue=0;
+            int divider=0;
             
-            // for corners (divide by 4)
-            if(i==0 && j ==0){
-                //top left
-                for (int a = i; a<i+2; a++){
-                    for (int b = j; b<j+2; b++){
+            for(int a = i-1; a<i+2; a++){
+                for(int b = j-1; b<j+2; b++){
+
+                    // if the pizel is out of bound, skip it
+                    if ((a>=0 && a<=height-1) && (b>=0 && b<=width-1)){
                         sum_red += image[a][b].rgbtRed;
                         sum_blue += image[a][b].rgbtBlue;
                         sum_green += image[a][b].rgbtGreen;
+                        divider++; // count the pixels that need to be divided to
                     }
                 }
-
-                rounded.rgbtRed = round(sum_red/4.0);
-                rounded.rgbtGreen = round(sum_green/4.0);
-                rounded.rgbtBlue = round(sum_blue/4.0);
-
-                temp[i][j] = rounded;
             }
-            else if (i==0 && j== width-1){
-                //top right
-                for (int a = i; a<i+2; a++){
-                    for (int b = j-1; b<j+1; b++){
-                        sum_red += image[a][b].rgbtRed;
-                        sum_blue += image[a][b].rgbtBlue;
-                        sum_green += image[a][b].rgbtGreen;
-                    }
-                }
+            
+            rounded.rgbtRed = round(sum_red/divider);
+            rounded.rgbtGreen = round(sum_green/divider);
+            rounded.rgbtBlue = round(sum_blue/divider);
 
-                rounded.rgbtRed = round(sum_red/4.0);
-                rounded.rgbtGreen = round(sum_green/4.0);
-                rounded.rgbtBlue = round(sum_blue/4.0);
-
-                temp[i][j] = rounded;
-            }
-            else if (i==height-1 && j==0){
-                //bottom left
-                for (int a = i-1; a<i+1; a++){
-                    for (int b = j; b<j+2; b++){
-                        sum_red += image[a][b].rgbtRed;
-                        sum_blue += image[a][b].rgbtBlue;
-                        sum_green += image[a][b].rgbtGreen;
-                    }
-                }
-
-                rounded.rgbtRed = round(sum_red/4.0);
-                rounded.rgbtGreen = round(sum_green/4.0);
-                rounded.rgbtBlue = round(sum_blue/4.0);
-
-                temp[i][j] = rounded;
-            }
-            else if(i==height-1 && j==width-1){
-                //bottom right
-                for (int a = i-1; a<i+1; a++){
-                    for (int b = j-1; b<j+1; b++){
-                        sum_red += image[a][b].rgbtRed;
-                        sum_blue += image[a][b].rgbtBlue;
-                        sum_green += image[a][b].rgbtGreen;
-                    }
-                }
-
-                rounded.rgbtRed = round(sum_red/4.0);
-                rounded.rgbtGreen = round(sum_green/4.0);
-                rounded.rgbtBlue = round(sum_blue/4.0);
-
-                temp[i][j] = rounded;
-            }
-            // for edges (divide by 6)
-            else if (i==0 && 0<j<width-1){//edge top
-                for (int a = i; a<i+2; a++){
-                    for (int b = j-1; b<j+2; b++){
-                        sum_red += image[a][b].rgbtRed;
-                        sum_blue += image[a][b].rgbtBlue;
-                        sum_green += image[a][b].rgbtGreen;
-                    }
-                }
-
-                rounded.rgbtRed = round(sum_red/6.0);
-                rounded.rgbtGreen = round(sum_green/6.0);
-                rounded.rgbtBlue = round(sum_blue/6.0);
-
-                temp[i][j] = rounded;
-            }
-            else if (i==height-1 && 0<j<width-1){//edge bot
-                for (int a = i-1; a<i+1; a++){
-                    for (int b = j-1; b<j+2; b++){
-                        sum_red += image[a][b].rgbtRed;
-                        sum_blue += image[a][b].rgbtBlue;
-                        sum_green += image[a][b].rgbtGreen;
-                    }
-                }
-
-                rounded.rgbtRed = round(sum_red/6.0);
-                rounded.rgbtGreen = round(sum_green/6.0);
-                rounded.rgbtBlue = round(sum_blue/6.0);
-
-                temp[i][j] = rounded;
-            }
-            else if (0<i<height-1 && j == 0){//edge left
-                for (int a = i-1; a<i+2; a++){
-                    for (int b = j; b<j+2; b++){
-                        sum_red += image[a][b].rgbtRed;
-                        sum_blue += image[a][b].rgbtBlue;
-                        sum_green += image[a][b].rgbtGreen;
-                    }
-                }
-
-                rounded.rgbtRed = round(sum_red/6.0);
-                rounded.rgbtGreen = round(sum_green/6.0);
-                rounded.rgbtBlue = round(sum_blue/6.0);
-
-                temp[i][j] = rounded;
-            }
-            else if (0<i<height-1 && j == width-1){//edge right
-                for (int a = i-1; a<i+2; a++){
-                    for (int b = j-1; b<j+1; b++){
-                        sum_red += image[a][b].rgbtRed;
-                        sum_blue += image[a][b].rgbtBlue;
-                        sum_green += image[a][b].rgbtGreen;
-                    }
-                }
-
-                rounded.rgbtRed = round(sum_red/6.0);
-                rounded.rgbtGreen = round(sum_green/6.0);
-                rounded.rgbtBlue = round(sum_blue/6.0);
-
-                temp[i][j] = rounded;
-            }
-            // for middles (divide by 9)
-            else{
-                for (int a = i-1; a<i+2; a++){
-                    for (int b = j-1; b<j+2; b++){
-                        sum_red += image[a][b].rgbtRed;
-                        sum_blue += image[a][b].rgbtBlue;
-                        sum_green += image[a][b].rgbtGreen;
-                    }
-                }
-
-                rounded.rgbtRed = round(sum_red/9.0);
-                rounded.rgbtGreen = round(sum_green/9.0);
-                rounded.rgbtBlue = round(sum_blue/9.0);
-
-                temp[i][j] = rounded;
-            }
+            temp[i][j] = rounded;
         }
     }
 
@@ -231,6 +108,107 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
+    // compute Gx and Gy of the pixel
+    // ignore non-existence pixels (corners and egdes cases)
+    // the new value = root( Gx^2 + Gy^2 )
+    // squaring makes a positive value
+    // if value > 255, set it to 255
+
+    //intit Gx and Gy array
+    int Gx[3][3] = {{-1,0,1},{-2,0,2},{-1,0,1}};
+    int Gy[3][3] = {{-1,-2,-1},{0,0,0},{1,2,1}};
     
+    // init total sobel value of Gx and Gy
+    typedef struct 
+    {
+        float red;
+        float green;
+        float blue;
+    }
+    sobel;
+    
+    sobel sobel_x, sobel_y;
+
+    RGBTRIPLE holder[height][width];
+    int temp;
+
+    int count_i, count_j;
+    int x,y;
+
+    //loop over all pixels
+    for(int i=0; i<height; i++){
+        for(int j=0; j<width; j++){
+
+            //reset sobel values to 0
+            sobel_x.red = sobel_x.green = sobel_x.blue = 0;
+            sobel_y.red = sobel_y.green = sobel_y.blue = 0;
+
+            count_i = count_j = 0;
+            // 3x3 pixel grid calulation
+            for(int a = i-1; a<i+2; a++){
+                for(int b = j-1; b<j+2; b++){
+                    
+                    x = Gx[count_i][count_j];
+                    y = Gy[count_i][count_j];
+                    
+                    //** TODO: fix Gx and Gy implementation
+                    //   DONE
+                    //? Please check
+                    if ((a>=0 && a<=height-1) && (b>=0 && b<=width-1)){
+                        sobel_x.red += image[a][b].rgbtRed * x;
+                        sobel_x.green += image[a][b].rgbtGreen * x;
+                        sobel_x.blue += image[a][b].rgbtBlue * x;
+
+                        sobel_y.red += image[a][b].rgbtRed * y;
+                        sobel_y.green += image[a][b].rgbtGreen * y;
+                        sobel_y.blue += image[a][b].rgbtBlue * y;
+                    }
+                    count_j++;
+                }
+                count_i++;
+                count_j = 0;
+            }
+            
+            // TODO: fix holder
+
+            // holder for red channel
+           temp = round(sqrt((pow(sobel_x.red,2)) + (pow(sobel_y.red,2))));
+
+            if(temp > 255){
+                holder[i][j].rgbtRed = 255;
+            }
+            else{
+                holder[i][j].rgbtRed = temp;
+            }
+            
+            //holder for green channel
+            temp = round(sqrt(pow(sobel_x.blue,2) + pow(sobel_y.blue,2)));
+
+            if(temp > 255){
+                holder[i][j].rgbtBlue = 255;
+            }
+            else{
+                holder[i][j].rgbtBlue = temp;
+            }
+
+            //holder for blue channel
+            temp = round(sqrt(pow(sobel_x.green,2) + pow(sobel_y.green,2)));
+
+            if(temp > 255){
+                holder[i][j].rgbtGreen = 255;
+            }
+            else{
+                holder[i][j].rgbtGreen = temp;
+            }
+        }
+    }
+
+    // apply holders data into image
+    for (int i=0; i < height; i++){
+        for (int j=0; j < width; j++){
+            image[i][j] = holder[i][j];
+        }
+    }
+
     return;
 }
