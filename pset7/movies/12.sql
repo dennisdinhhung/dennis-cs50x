55069cbaaccf32a9efc2aea260b71c7ea6ac5a32
Dennis Dinh Hung <dennisdinhhung@gmail.com>
2021-09-04 01:58:44 +0700

    finished

---
+++ pset7/movies/12.sql
@@ -0,0 +1,5 @@
+SELECT DISTINCT(title) FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id WHERE name = "Johnny Depp"
+
+INTERSECT
+
+SELECT DISTINCT(title) FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id WHERE name = "Helena Bonham Carter";
---
+++ pset7/movies/13.sql
@@ -0,0 +1,1 @@
+SELECT name FROM people WHERE id IN (SELECT person_id FROM stars WHERE movie_id IN (SELECT movies.id FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id WHERE name = "Kevin Bacon")) EXCEPT SELECT name FROM people WHERE name = "Kevin Bacon";
